f679ffaa191ae2b8045af06e0821d500b4cefcb7
shohei-sho <shohei.seto@tcs.com>
2026-06-12 12:00:51 +0900

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 def add(a,b,c=0):
-  try:
-    return int(a+b) + int(c)
-  except:
-    return "error"
+  if type(a) != str || type(b) != str || type(c) != str:
+    if 0 <= a <= 10 and 0 <= b <= 10 and 0 <= c <= 10 
+      return int(a+b) + int(c)
+    else:
+      return -1
+  else:
+    return -1
